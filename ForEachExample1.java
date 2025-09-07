7e168ceeb209092e08f136084bb1fccf3d9dc6f5
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-09-07 14:34:12 +0400

    Add files via upload

---
+++ ForEachExample1.java
@@ -0,0 +1,19 @@
+package yenipabka;
+
+public class ForEachExample1 {
+
+    public static void main(String[] args) {
+        int[] ededler = {1, 2, 3, 4, 5};
+
+
+for (int eded : ededler) {
+    System.out.println(eded);
+}
+
+
+
+
+    }
+
+
+}
